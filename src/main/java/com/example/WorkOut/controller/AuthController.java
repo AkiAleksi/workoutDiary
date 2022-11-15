075b9c5030b03cd116c2530fa05271e953a09b70
AkiAleksi <aki@hietamaki.org>
2022-11-15 14:55:36 +0200

    bug fix 100

--- src/main/java/com/example/WorkOut/controller/AuthController.java
+++ src/main/java/com/example/WorkOut/controller/AuthController.java
@@ -48,14 +48,12 @@ public class AuthController {
 	@PostMapping("/register/save")
 	public String registration(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result, Model model) {
 		User existingUser = userService.findUserByEmail(userDto.getEmail());
-
-		if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
-			result.rejectValue("email", null, "There is already an account registered with the same email");
-		}
-
 		if (result.hasErrors()) {
 			model.addAttribute("user", userDto);
 			return "/register";
+		}
+		if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+			result.rejectValue("email", null, "There is already an account registered with the same email");
 		}
 
 		userService.saveUser(userDto);
